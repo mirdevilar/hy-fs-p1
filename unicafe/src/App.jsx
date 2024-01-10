0bfa88e9a1e43d1b0673c40bdd9035639251c51d
mirdevilar <dev@mirdevilar.com>
2024-01-10 22:29:08 +0200

    1.6

---
+++ unicafe/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+)
+
+const Counter = ({count, name}) => (
+  <p>{name} {count}</p>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Counter count={good} name="good" />
+      <Counter count={neutral} name="neutral" />
+      <Counter count={bad} name="bad" />
+    </>
+  )
+}
+
+export default App
